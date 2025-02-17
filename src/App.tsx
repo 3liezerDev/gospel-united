144fc0e71461e320d6debbc93bbe113b5cee6131
3liezerDev <3dgar3liezer@gmail.com>
2025-02-17 14:47:57 +0100

    test diferent types of buttons

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,26 @@ function App() {
       <h1 className='text-3xl text-center font-bold underline text-blue-700'>
         Hello you're working with Tailwind css 4
       </h1>
-      <Button variant="default">
-        Click me please
-      </Button>
+      <section className="flex flex-col gap-4 justify-center items-center">
+        <Button variant="default">
+          Click me please
+        </Button>
+        <Button variant="destructive">
+          Click me please
+        </Button>
+        <Button variant="outline">
+          Click me please
+        </Button>
+        <Button variant="secondary">
+          Click me please
+        </Button>
+        <Button variant="ghost">
+          Click me please
+        </Button>
+        <Button variant="link">
+          Click me please
+        </Button>
+      </section>
     </>
   )
 }
