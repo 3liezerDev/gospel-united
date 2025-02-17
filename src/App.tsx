f6fea6fc01a82b77ccbf1347e0c6d62ee3748106
3liezerDev <3dgar3liezer@gmail.com>
2025-02-17 14:35:58 +0100

    shadcn installation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 
+import { Button } from "@/components/ui/button"
+
 function App() {
  
 
@@ -7,6 +9,9 @@ function App() {
       <h1 className='text-3xl text-center font-bold underline text-blue-700'>
         Hello you're working with Tailwind css 4
       </h1>
+      <Button variant="default">
+        Click me please
+      </Button>
     </>
   )
 }
