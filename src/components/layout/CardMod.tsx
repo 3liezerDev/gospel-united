e4341908668c7f126c737ac588301aa9b2b45bd3
3liezerDev <3dgar3liezer@gmail.com>
2025-02-18 00:47:50 +0100

    adding first version of responsive in custom carousel

--- src/components/layout/CardMod.tsx
+++ src/components/layout/CardMod.tsx
@@ -30,40 +30,41 @@
    
   export function CardMod({ className, ...props }: CardProps) {
     return (
-      <article className="flex flex-row justify-center  w-full gap-5">
-      <img className="w-xs object-cover rounded-md"  src="src\assets\images\light.webp" alt="" />
-      <Card className={cn("w-lg", className)} {...props}>
-        <CardHeader>
-          <CardTitle>Notifications</CardTitle>
-          <CardDescription>You have 3 unread messages.</CardDescription>
-        </CardHeader>
-        <CardContent className="grid gap-4">
-          <div>
-            {notifications.map((notification, index) => (
-              <div
-                key={index}
-                className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0"
-              >
-                <span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
-                <div className="space-y-1">
-                  <p className="text-sm font-medium leading-none">
-                    {notification.title}
-                  </p>
-                  <p className="text-sm text-muted-foreground">
-                    {notification.description}
-                  </p>
+      <article className="flex flex-col md:flex-row justify-center w-full gap-4 md:gap-5">
+        <img
+          className="w-full md:w-xs lg:w-md object-cover rounded-md"
+          src="src/assets/images/light.webp"
+          alt=""
+        />
+        <Card className={cn("w-full md:w-lg", className)} {...props}>
+          <CardHeader>
+            <CardTitle>Notifications</CardTitle>
+            <CardDescription>You have 3 unread messages.</CardDescription>
+          </CardHeader>
+          <CardContent className="grid gap-4">
+            <div>
+              {notifications.map((notification, index) => (
+                <div
+                  key={index}
+                  className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0"
+                >
+                  <span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
+                  <div className="space-y-1">
+                    <p className="text-sm font-medium leading-none">
+                      {notification.title}
+                    </p>
+                    <p className="text-sm text-muted-foreground">
+                      {notification.description}
+                    </p>
+                  </div>
                 </div>
-              </div>
-            ))}
-          </div>
-        </CardContent>
-        <CardFooter>
-          <Button className="w-full">
-            Mark
-          </Button>
-        </CardFooter>
-        
-      </Card>
+              ))}
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button className="w-full">Mark</Button>
+          </CardFooter>
+        </Card>
       </article>
-    )
+    );
   }
