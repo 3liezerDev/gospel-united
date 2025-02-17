90afd98a067f810fade3ac429337b8b34d2cde40
3liezerDev <3dgar3liezer@gmail.com>
2025-02-17 13:59:01 +0100

    vit config and tailwind 4

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1 className='text-3xl text-center font-bold underline text-blue-700'>
+        Hello you're working with Tailwind css 4
+      </h1>
+    </>
+  )
+}
+
+export default App
