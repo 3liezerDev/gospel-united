85d837bebde97e2a5651acf4af90f0985ec936b9
3liezerDev <3dgar3liezer@gmail.com>
2025-04-04 02:08:41 +0200

    refactor(ui): create reusable hover card component and separate image data for gallery and carousel

---
+++ src/lib/carouselImages.ts
@@ -0,0 +1,38 @@
+export const carouselImages = [
+    {
+      imageSrc: "/assets/images/carousel/grupo-danza.jpg",
+      imageAlt: "Imagen de Danza",
+      title: "Danza",
+      subtitle: "Expresando la adoración por medio de la danza ",
+      notifications: [
+        { title: "Coreografía especial de grupo de danza", description: "Grupo oficial del evento" },
+        { title: "Participaciones especiales", description: "Grupos invitados" },
+        { title: "Presentación oficial en evento gospel united", description: "Sábado 03 de Mayo 2025" },
+      ],
+      buttonText: "Ver Galería",
+    },
+    {
+      imageSrc: "/assets/images/carousel/danza-alabanza01.jpg",
+      imageAlt: "Imagen de Alabanza",
+      title: "Alabanza y Adoración",
+      subtitle: "Elevando nuestras voces en gratitud",
+      notifications: [
+        { title: "Servicio de adoración en vivo", description: "Salmo 100:2 Adoren al Señor con alegría; vengan ante él con cánticos de júbilo." },
+        { title: "Interpretación de alabanza y adoración", description: "Interpretación musical en vivo" },
+        { title: "Participaciones especiales", description: "Cantantes invitados" },
+      ],
+      buttonText: "Ver Galería",
+    },
+    {
+      imageSrc: "/assets/images/carousel/pastores-01.jpg",
+      imageAlt: "Imagen de Participaciones Especiales",
+      title: "Participaciones Especiales",
+      subtitle: "Momentos únicos de inspiración y fortalecimiento",
+      notifications: [
+        { title: "Testimonios impactantes", description: "obras maravillosas de Dios" },
+        { title: "Invitados especiales confirmados", description: "Pastores y predicadores" },
+        { title: "Presentaciones sorpresa", description: "Teatros, Cantos y más.." },
+      ],
+      buttonText: "Ver Galería",
+    },
+  ];
