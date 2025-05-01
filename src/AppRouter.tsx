6a39929ba1463bc14bf995e9b6dd44e4a72b8e96
3liezerDev <3dgar3liezer@gmail.com>
2025-05-01 22:34:37 +0200

    feat: add <Toaster /> from sonner to AppRouter with top-center position

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -4,19 +4,22 @@ import Layout from "./components/layout/Layout";
 import Form from './pages/Form'; 
 import UserList from './pages/UserList'; 
 import Collaborators from "./pages/Collaborators";
+import { Toaster } from "./components/ui/sonner";
+
 const AppRouter = () => {
   return (
     <Router>
+      <Toaster position="top-center"/>
       <Routes>
+        {/* Rutas fuera del layout */}
         <Route path="form" element={<Form />} />
-         <Route path="users" element={<UserList />} />
+        <Route path="users" element={<UserList />} />
 
-        <Route element={<Layout />}> 
-          <Route index path="/" element={<Home />} />
-          <Route index path="colaboradores" element={<Collaborators />} />
-          
+        {/* Rutas que usan el layout */}
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} /> {/* Ruta raíz */}
+          <Route path="colaboradores" element={<Collaborators />} />
         </Route>
-
       </Routes>
     </Router>
   );
